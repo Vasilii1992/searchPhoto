4dd7f9986e901e17bdb1e3891daf17627a6d25e9
Vasilii Tikhonov <tihonovvasilij154@gmail.com>
2024-07-27 15:09:19 +0300

    Finished the network layer

--- Photo search/Service/NetworkService.swift
+++ Photo search/Service/NetworkService.swift
@@ -11,13 +11,23 @@ import Foundation
 class NetworkService {
     
     
-    func request(searchTer: String, completion: (Data?, Error) -> Void) {
+    func request(searchTer: String, completion: @escaping (Data?, Error?) -> Void) {
         let parameters = prepareParameters(searchTerm: searchTer)
         let url = url(params: parameters)
-        print(url)
-        
+        var request = URLRequest(url: url)
+        request.allHTTPHeaderFields = prepareHeader()
+        request.httpMethod = "get"
+        let task = createDataTask(from: request, completion: completion)
+        task.resume()
         
     }
+    
+    private func prepareHeader() -> [String: String]? {
+        var headers = [String: String]()
+        headers["Authorization"] = "Client-ID 0VHkF_7JwEHmI__tYFqCB_S5EqJEOkR_X4szoxzw4yA"
+        return headers
+    }
+    
     
     private func prepareParameters(searchTerm: String?) -> [String: String] {
         var parameters = [String: String]()
@@ -34,5 +44,15 @@ class NetworkService {
         components.path = "/search/photos"
         components.queryItems = params.map { URLQueryItem(name: $0, value: $1) }
         return components.url!
+    }
+    
+    
+    private func createDataTask(from request: URLRequest, completion: @escaping (Data?, Error?) -> Void) -> URLSessionDataTask {
+        
+        return URLSession.shared.dataTask(with: request) { data, response, error in
+            DispatchQueue.main.async {
+                completion(data, error)
+            }
+        }
     }
 }
